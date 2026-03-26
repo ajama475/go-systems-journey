299e7ba23a823391b7808152cdc65afb21c8d9a4
Ajax <ahmedjama475@gmail.com>
2026-03-26 00:19:39 -0600

    add comment

--- gobasic3/main.go
+++ gobasic3/main.go
@@ -1,3 +1,10 @@
+/*
+This program reads in words, count how many times the
+word shows up and stores them using key value pair. 
+It also sorts them in descending order using the number of
+times the word showed up
+*/
+
 package main
 
 import (
@@ -15,7 +22,7 @@ func main() {
 	scan.Split(bufio.ScanWords)
 
 	for scan.Scan() {
-		//increment a word when scanned
+		//increment a word when read
 		words[scan.Text()]++
 	}
 	fmt.Println(len(words), "unique words")
